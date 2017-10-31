3b77be9670bfbf346a22f4cd8fec30c5ae79f041
Matt <m_dittmann@yahoo.com>
2017-10-30 22:21:28 -0700

    update js

--- js/event-handler.js
+++ js/event-handler.js
@@ -1,11 +1,18 @@
 function checkUsername() {
-  var elMsg = document.getelementById('feedback');
+  // GET FEEDBACK ELEMENT
+  var elMsg = document.getElementById('feedback');
+  // IF USERNAME IS TOO SHORT
   if (this.value.length < 5) {
+    // SET MESSAGE
     elMsg.textContent = "Username must be 5 characters or more";
+    // IF USERNAME IS NOT TOO SHORT
   } else {
+    // CLEAR THE MESSAGE
     elMsg.textContent = "";
   }
 }
 
+// GET THE INPUT VALUE FROM THE USERNAME FIELD
 var elUsername = document.getElementById('username');
+// WHEN THE FIELD LOSES FOCUS, CALL checkUsername()
 elUsername.onblur = checkUsername;
